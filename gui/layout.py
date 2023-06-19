31ba836aa9ed9e62c4b1a834bb5d2ebcccf0e21a
matthewdutton1983 <79879742+matthewdutton1983@users.noreply.github.com>
2023-06-18 21:05:19 -0400

    Update layout.py

--- gui/layout.py
+++ gui/layout.py
@@ -3,22 +3,14 @@ import streamlit as st
 
 
 class Layout:
-
     def show_header(self):
-        """
-        Displays the header of the app
-        """
-        st.markdown(
-            """
-            <h1 style='text-align: center;'>Chat with your PDF!</h1>
-            """,
-            unsafe_allow_html=True,
-        )
+        """Displays the header of the app"""
+        st.header("Unleash the power of conversational AI with Doc-GPT")
+        st.subheader(
+            "Chat with your PDF documents to get instant insights")
 
     def show_api_key_missing(self):
-        """
-        Displays a message if the user has not entered an API key
-        """
+        """Displays a message if the user has not entered an API key"""
         st.markdown(
             """
             <div style='text-align: center;'>
@@ -29,9 +21,7 @@ class Layout:
         )
 
     def prompt_form(self):
-        """
-        Displays the prompt form
-        """
+        """Displays the prompt form"""
         with st.form(key="my_form", clear_on_submit=True):
             user_input = st.text_area(
                 "Query:",
@@ -42,4 +32,5 @@ class Layout:
             submit_button = st.form_submit_button(label="Send")
 
             is_ready = submit_button and user_input
+
         return is_ready, user_input
